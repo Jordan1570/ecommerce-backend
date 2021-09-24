3962e2d13248b61d869ccd47a530d53ef3f1ddf7
Jordan Scott <jscott0751@gmail.com>
2021-09-23 21:40:58 -0400

    completed model relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  unique: false
+});
 // Categories have many Products
-
+Catergories.hasMany(Product, {
+  foreignKey: 'category_id',
+  unique: false,
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
 
+  foreignKey: 'tag_id',
+  unique: false 
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
 
+  foreignKey: 'tag_id',
+  unique: false
+})
 module.exports = {
   Product,
   Category,
