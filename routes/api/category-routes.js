f2bb1df8269c6d9b44098586c71ef6dbae9d6063
Jordan Scott <jscott0751@gmail.com>
2021-09-24 18:00:17 -0400

    seeded tables properly

---
+++ routes/api/category-routes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { Category, Product, ProductTag } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    include: [ 
+      {
+        model: Product,
+      }
+    ]
+  })
+    .then((Categories) => res.status(200).json(Categories))
+    .catch((err) => res.status(500).json(err));
+});
+
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [ 
+      {
+        model: Product,
+      },
+    ],
+  })
+    .then((Category) => res.status(200).json(Category))
+    .catch((err) => res.status(500).json(err));
+});
+
+
+
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create(req.body,)
+    .then((Category) => res.status(200).json(Category))
+    .catch((err) => res.status(500).json(err));
+});
+
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((Category) => res.status(200).json(Category))
+  .catch((err) => res.status(500).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((Category) => res.status(200).json(Category))
+  .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
