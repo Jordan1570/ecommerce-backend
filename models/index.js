f510021f9d0de3599f09c148ecbcd9db8a2147aa
Jordan Scott <jscott0751@gmail.com>
2021-09-24 20:53:31 -0400

    tested routes, ecommerce db completed

--- models/index.js
+++ models/index.js
@@ -7,12 +7,17 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  unique: false
+  unique: false,
+
+  onDelete: 'CASCADE',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   unique: false,
+
+  onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
